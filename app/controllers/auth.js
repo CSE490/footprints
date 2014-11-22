57bdc356e48e428453d187f5f7c311ee0a13ca0d
Christopher Su <christophersu9@gmail.com>
2014-11-22 12:08:24 -0800

    working auth!

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -3,12 +3,41 @@ var express = require('express'),
   mongoose = require('mongoose'),
   passport = require('passport');
 
+var User = require('../models/user');
+
 module.exports = function (app) {
   app.use('/', router);
 };
 
-router.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
-);
+// app.get('/register', function(req, res) {
+//   res.render('register', { });
+// });
+
+router.post('/register', function(req, res) {
+  User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
+      if (err) {
+          return res.render('register', { account : account });
+      }
+
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/');
+      });
+  });
+});
+
+// app.get('/login', function(req, res) {
+//     res.render('login', { user : req.user });
+// });
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+    res.send("pong!", 200);
+});
--- config/express.js
+++ config/express.js
@@ -11,21 +11,11 @@ var session = require('express-session')
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var User = require('../app/models/user');
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 module.exports = function(app, config) {
   app.set('views', config.root + '/app/views');
